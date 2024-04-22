e7f6ae58982a0a3796ff2f9f1fdc3aa3463a175f
ojea <ojeaxe@gmail.com>
2024-04-22 13:10:02 +0200

    Add project files.

---
+++ MvcDockersComics/Repositories/RepositoryComics.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcDockersComics.Data;
+using MvcDockersComics.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MvcDockersComics.Repositories
+{
+    public class RepositoryComics
+    {
+        private ComicsContext context;
+
+        public RepositoryComics(ComicsContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Comic>> GetComicsAsync()
+        {
+            return await this.context.Comics.ToListAsync();
+        }
+        private async Task<int> GetMaxIdAsync()
+        {
+            return await this.context.Comics.MaxAsync(z => z.IdComic) + 1;
+        }
+
+        public async Task InsertComic(string nombre, string imagen)
+        {
+            Comic comic = new Comic();
+            comic.IdComic = await this.GetMaxIdAsync();
+            comic.Nombre = nombre; 
+            comic.Imagen = imagen;
+            this.context.Comics.Add(comic);
+            await this.context.SaveChangesAsync();
+        }
+
+    }
+}
